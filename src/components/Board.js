7746cee5c5c4c43cd0376706a0efa04bd4eb09d3
TANJE5 <jepthe.tan@oocl.com>
2022-09-12 17:49:01 +0800

    feat: passed function from Board to Square

--- src/components/Board.js
+++ src/components/Board.js
@@ -11,7 +11,10 @@ class Board extends React.Component {
     }
 
     renderSquare(i) {
-        return <Square value={i} />;
+        return <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />;
     }
 
     render() {

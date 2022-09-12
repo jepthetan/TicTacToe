d29498f2eb97c841d2aee8ed4507ae2fdb10c6ef
TANJE5 <jepthe.tan@oocl.com>
2022-09-12 17:42:31 +0800

    feat: add constructor for Square Class

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
         return (
             <button className="square" onClick={() => console.log('click')}>
